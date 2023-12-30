eeba92f6b9e005ede3141850dc63cd7eeb7afa9b
majkus522 <mikohus522@gmail.com>
2023-12-30 22:43:04 +0100

    Guild ANTI-DELETE
    Refers to majkus522/m-rpg-website@07978fc

--- mysql/database.sql
+++ mysql/database.sql
@@ -46,9 +46,9 @@ CREATE TABLE `guilds`
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `slug` varchar(100) NOT NULL UNIQUE,
-  `leader` int(11) UNSIGNED NOT NULL UNIQUE,
+  `leader` int(11) UNSIGNED NULL UNIQUE,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`leader`) REFERENCES `players`(id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (`leader`) REFERENCES `players`(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE `password-recovery`
