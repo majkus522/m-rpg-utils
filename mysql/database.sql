b30f77d5f2f8f752452abec4a889fd700335feb1
majkus522 <mikohus522@gmail.com>
2024-01-24 21:18:06 +0100

    Optimize json-property function

--- mysql/database.sql
+++ mysql/database.sql
@@ -105,9 +105,9 @@ FROM `players`
 LEFT JOIN `guilds` ON `guilds`.`id` = `players`.`guild`;
 
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` FUNCTION `json_property`(`root` TEXT CHARSET utf8, `file` TEXT CHARSET utf8, `property` TEXT CHARSET utf8) RETURNS TEXT CHARSET utf8 COLLATE utf8_bin
+CREATE DEFINER=`root`@`localhost` FUNCTION `json_property`(`file` TEXT CHARSET utf8, `property` TEXT CHARSET utf8) RETURNS TEXT CHARSET utf8 COLLATE utf8_bin
     DETERMINISTIC
 begin
-    return replace(json_extract(convert(LOAD_FILE(concat(root, "/", file, ".json")) using utf8mb4), concat("$.", property)), '"', "");
+    return replace(json_extract(convert(LOAD_FILE(concat("E:/xampp/htdocs/m-rpg/api/data/", file, ".json")) using utf8mb4), concat("$.", property)), '"', "");
 end$$
-DELIMITER ;
+DELIMITER;
